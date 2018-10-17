60f3c20d44a5738af9f21b8a796a968024d40cfc
Sterling Anderson <sterling.anderson@wisc.edu>
2018-10-17 10:18:07 -0500

    updates

--- check_wp.rb
+++ check_wp.rb
@@ -9,42 +9,60 @@ OptionParser.new do |opt|
 	opt.on('-u WWWUSER', '--wwwuser=WWWUSER', 'User web server runs as') { |o| options[:server_user] = o }
 	opt.on('-d DOCROOT', '--docroot=DOCROOT', 'Document root') { |o| options[:doc_root] = o }
 	opt.on('-p', '--plugins', 'Check plugins') { |o| options[:plugins] = true }
+	opt.on('-t', '--themes', 'Check themes') { |o| options[:themes] = true }
+	opt.on('-c', '--count', 'Show number of updates available') { |o| options[:count] = true }
+	opt.on('-l', '--list', 'List updates available') { |o| options[:list] = true }
 end.parse!
 
-if options[:plugins] == true
-	#check for plugin updates
-	updates_command="/usr/bin/sudo -u " + options[:server_user] + " -i -- /usr/local/bin/wp plugin list --path=" + options[:doc_root] + "  --update=available --format=count"
-	call_command="/usr/bin/sudo -u " + options[:server_user] + " -i -- /usr/local/bin/wp plugin list --path=" + options[:doc_root] + " --update=available --format=json"
-	call_command="/usr/bin/sudo -u " + options[:server_user] + " -i -- /usr/local/bin/wp plugin list --path=" + options[:doc_root] + " --update=available --format=json --fields=title,name,update"
-	updates_result=`#{updates_command}`
+if (options[:plugins] && options[:themes])
+	puts "Pick plugins or themes, not both"
+	exit 1
+end
 
-	case
-	when updates_result.to_i == 0
-		puts "OK: all plugins up to date."
+if (options[:plugins] || options[:themes])
+	if options[:plugins] == true
+		to_check = "plugin"
+	elsif options[:themes] == true
+		to_check = "theme"
+	end
+	updates_command="/usr/bin/sudo -u " + options[:server_user] + " -i -- /usr/local/bin/wp " + to_check + " list --path=" + options[:doc_root] + "  --update=available --format=count"
+	updates_result=`#{updates_command}`
+	if options[:count] == true
+		puts updates_result.to_i
 		exit 0
-	when updates_result.to_i >= 1
-		call_result=`#{call_command}`
-		result=JSON.parse(call_result,{:symbolize_names=>true})
-		name_list=result.collect{|x| x[:title]}
-		puts "WARNING: " + updates_result + " plugin " + "update".pluralize(updates_result.to_i) + " available (" + name_list.join(", ") + ")"
-	else
-		puts "UNKNOWN: No valid update count returned"
-		exit 3
+	end
+	if options[:list] == true 
+		case
+		when updates_result.to_i == 0
+			exit 0
+		when updates_result.to_i >= 1
+			call_command="/usr/bin/sudo -u " + options[:server_user] + " -i -- /usr/local/bin/wp " + to_check + " list --path=" + options[:doc_root] + " --update=available --format=json --fields=title,name,update"
+			call_result=`#{call_command}`
+			result=call_result && call_result.length >= 2 ? JSON.parse(call_result,{:symbolize_names=>true}) : result
+			name_list=result && call_result.length >= 2 ? result.collect{|x| x[:title]} : result[:name]
+			puts name_list.join(", ")
+			exit 0
+		end
 	end
 else
 	#check for core updates
 	updates_command="/usr/bin/sudo -u " + options[:server_user] + " -i -- /usr/local/bin/wp core check-update --path=" + options[:doc_root] + "  --format=count"
-        call_command="/usr/bin/sudo -u " + options[:server_user] + " -i -- /usr/local/bin/wp plugin list --path=" + options[:doc_root] + " --update=available --field=title"
-
-        updates_result=`#{updates_command}`
-
-        case
-        when updates_result.to_i == 0
-                puts "OK: Wordpress is up to date."
-                exit 0
-        when updates_result.to_i >= 1
-                puts "WARNING: " + updates_result + " plugin " + "update".pluralize(updates_result.to_i) + " available"
-        else
-                puts "UNKNOWN: No valid update count returned"
+	updates_result=`#{updates_command}`
+	if options[:count] == true
+		puts updates_result.to_i
+		exit 0
+	end
+	if options[:list] == true
+		case
+		when updates_result.to_i == 0
+			exit 0
+		when updates_result.to_i >= 1
+			call_command="/usr/bin/sudo -u " + options[:server_user] + " -i -- /usr/local/bin/wp core check-update --path=" + options[:doc_root] + " --format=json"
+			call_result=`#{call_command}`
+			result=JSON.parse(call_result,{:symbolize_names=>true})
+			version_list=result.collect{|x| x[:version]}
+			puts version_list
+			exit 0
+		end
 	end
 end
